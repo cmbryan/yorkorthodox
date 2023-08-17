3cef8d743c0cb9c3f18e0a21859dc0f530add2b0
Chris Bryan <chrismbryan@gmail.com>
2023-08-17 08:13:26 +0100

    making room for rest server

--- site/run.sh
+++ site/run.sh
@@ -5,4 +5,7 @@ SITE=./www
 pip3 install --user -r requirements.txt
 mkdir -p $SITE
 python3 -m http.server -d $SITE &
-exec staticjinja watch --static static --outpath $SITE
+while true; do
+    python -m staticjinja watch --static static --outpath $SITE
+    sleep 1
+done
