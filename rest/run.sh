0329e497ecb3f8d0665caf29b4cf28fcc2731162
Chris Bryan <chrismbryan@gmail.com>
2023-08-25 09:49:35 +0100

    working fly deployment

--- rest/run.sh
+++ rest/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Python requirements
 pip3 install -r requirements.txt
+
+# Fly hosting CLI
+apt update && apt install -y curl
+curl -L https://fly.io/install.sh | sh
+echo export PATH="/root/.fly/bin:$PATH" >> ~/.bashrc
+
+# Run the server on :5000, restarting on failure
+# (Ctrl-C twice to exit)
 while true; do
     FLASK_APP=hellofly python -m flask run --host 0.0.0.0
     sleep 1
