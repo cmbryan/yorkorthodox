df7beb54302056c143648e1f2485998148d517e4
Chris Bryan <chrismbryan@gmail.com>
2023-09-08 14:22:48 +0000

    fix python path

--- rest/build.sh
+++ rest/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# This is only needed by render, which isn't using ../Dockerfile, as it's restricted to the rest sub-folder
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export PYTHONPATH=$SCRIPT_DIR/..
+python -m pip install -r ${SCRIPT_DIR}/requirements.txt
+
 cd calendar
 ./_build.sh
+
 cd ../scripts
 ./generate_service_db.py
