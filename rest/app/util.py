b544d9e896a350e0620c49770b5b125c7a72a3ce
Chris Bryan <chrismbryan@gmail.com>
2023-09-09 14:17:22 +0000

    fix services

--- rest/app/util.py
+++ rest/app/util.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 
 
 class NoDataException(Exception):
@@ -43,38 +43,40 @@ def build_date_str(data: Dict[str, str]) -> str:
     return f'{data["day_name"]}, {data["ord"]} {data["month"]} {data["year"]}'
 
 
-def query(database: str, query_txt: str, args=tuple(), fetchall=False) -> Tuple:
+def query(
+    database: str, query_txt: str, args=tuple(), fetchall=False
+) -> Union[List, Tuple]:
     """Perform an SQL query and return some data, or throw an exception."""
     with sqlite3.connect(database) as conn:
         cursor = conn.cursor()
         cursor.execute(query_txt, args)
-        result = cursor.fetchall if fetchall else cursor.fetchone()
+        result = cursor.fetchall() if fetchall else cursor.fetchone()
 
-    if not result or not isinstance(result, tuple):
-        raise NoDataException()
+    if not result:
+        raise NoDataException(f"{database}: '{query_txt}' {args}")
 
     return result
 
 
 def get_data_dict(date: str) -> Dict[str, str]:
     """Get the data for everything except the readings into a dictionary with table-name keys"""
-    query_result = query(lectionary_db_path, "SELECT * FROM yocal_main WHERE date = ?", (date,))
+    query_result = query(
+        lectionary_db_path, "SELECT * FROM yocal_main WHERE date = ?", (date,)
+    )
     data = dict(zip(main_columns, query_result))
     return data
 
 
 def get_services(date: str, num_services: int) -> List[Dict[str, str]]:
     """Get the next N services"""
-    results = (
-        query(
-            services_db_path,
-            'SELECT date_str, commemoration, Group_Concat(d.text, ", ")'
-            " FROM services s JOIN descriptions d ON d.service_id = s.id"
-            " WHERE timestamp >= ?"
-            " GROUP BY s.id"
-            " LIMIT ?",
-            (date, num_services),
-            fetchall=True,
-        ),
+    results = query(
+        services_db_path,
+        'SELECT date_str, commemoration, Group_Concat(d.text, ", ")'
+        " FROM services s JOIN descriptions d ON d.service_id = s.id"
+        " WHERE timestamp >= ?"
+        " GROUP BY s.id"
+        " LIMIT ?",
+        (date, num_services),
+        fetchall=True,
     )
-    return [dict(zip(["date", "commemoration", "description"], str(r))) for r in results]
+    return [dict(zip(["date", "commemoration", "description"], r)) for r in results]
