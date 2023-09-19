5917261d391c293253116f6a26013b0eb0d2c347
Chris Bryan <chrismbryan@gmail.com>
2023-09-19 15:59:41 +0000

    working date selection

--- site/templates/static/functions.js
+++ site/templates/static/functions.js
@@ -33,6 +33,12 @@ function getData(url, mode, callback) {
     xhr.send();
 }
 
+function getRestHost() {
+    var is_debug = window.location.host.startsWith("localhost") || window.location.host.startsWith("127.0.0.1");
+    var rest_host = is_debug ? "http://localhost:5000" : "https://yorkorthodox-rest-2.onrender.com";
+    return rest_host;
+}
+
 function setInnerHtmlIfExists(id, content) {
     var element = document.getElementById(id);
     if (element && content !== undefined) {
@@ -40,16 +46,9 @@ function setInnerHtmlIfExists(id, content) {
     }
 }
 
-
-// dynamic content
-document.addEventListener("DOMContentLoaded", function () {
-    var is_debug = window.location.host.startsWith("localhost") || window.location.host.startsWith("127.0.0.1");
-    var rest_host = is_debug ? "http://localhost:5000" : "https://yorkorthodox-rest-2.onrender.com";
-
-    var formattedDate = getTodaysDate();
-
+function setLectionary(formattedDate) {
     // Lectionary
-    getData(`${rest_host}/lectionary?date=${formattedDate}`,
+    getData(`${getRestHost()}/lectionary?date=${formattedDate}`,
         'GET',
         function (data) {
             var references = data.a_lect_1;
@@ -77,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     // Services
-    getData(`${rest_host}/services?date=${formattedDate}&num_services=5`,
+    getData(`${getRestHost()}/services?date=${formattedDate}&num_services=5`,
         'GET',
         function (data) {
             content = "";
@@ -93,73 +92,19 @@ document.addEventListener("DOMContentLoaded", function () {
             setInnerHtmlIfExists("services", content);
         }
     );
+}
+
+// dynamic content
+document.addEventListener("DOMContentLoaded", function () {
+    // Date Selector
+    const dateControl = document.querySelector('input[type="date"]');
+    if (dateControl) {
+        dateControl.value = getTodaysDate();
+    }
+
+    setLectionary(getTodaysDate());
 });
 
-// $(function() {
-//     $("#datepicker").datepicker({
-//         dateFormat: "yy-mm-dd",
-//         showOn: "both",
-//         buttonImageOnly: true,
-//         buttonImage: "calendar.svg",
-//         onSelect: function(date) {
-//             getLection(new Date(date));
-//         },
-//     }).datepicker("setDate", "0"); // initialize to today
-// });
-
-// var currDate;
-
-// function getLectionOffset(increment) {
-//     currDate.setDate(currDate.getDate() + increment);
-//     getLection(currDate);
-// }
-
-// function getLection(date) {
-//     currDate = date;
-//     dateStr = currDate.toISOString().slice(0, 10);
-//     fetch("/rest/daily/" + dateStr, {
-//             method: "GET",
-//             headers: {
-//                 Accept: "application/json",
-//                 "Content-Type": "application/json"
-//             }
-//         })
-//         .then(res => res.json())
-//         .then(data => {
-//             const day_json = data[0]
-//             document.getElementById("lection-date").innerHTML = day_json['date_str']
-//                 // document.getElementById("lection-tone").innerHTML = day_json['tone']
-//                 // document.getElementById("lection-desig-a").innerHTML = day_json['desig_a']
-//             document.getElementById("lection-general-commem").innerHTML = day_json['general_commem']
-//                 // document.getElementById("lection-british-commem").innerHTML = day_json['british_commem']
-//                 // ordering?
-//                 // todo bold the readings for liturgy
-//             titles = [day_json['a_lect_1']]
-//             document.getElementById('lection-a1').innerHTML = day_json['a_text_1']
-
-//             if (day_json['a_lect_2']) {
-//                 titles.push(day_json['a_lect_2'])
-//                 document.getElementById('lection-a2').innerHTML = day_json['a_text_2']
-//             }
-//             titles.push(day_json['g_lect'])
-//             document.getElementById('lection-g').innerHTML = day_json['g_text']
-//             if (day_json['x_lect_1']) {
-//                 titles.push(day_json['x_lect_1'])
-//             }
-//             if (day_json['x_lect_2']) {
-//                 titles.push(day_json['x_lect_2'])
-//             }
-//             if (day_json['c_lect_1']) {
-//                 titles.push(day_json['c_lect_1'])
-//             }
-//             if (day_json['c_lect_2']) {
-//                 titles.push(day_json['c_lect_2'])
-//             }
-//             document.getElementById("lection-titles").innerHTML = titles.join("; ")
-//                 // better?
-//                 // document.querySelector(
-//                 //     ".result"
-//                 // ).innerText = `The sum is: ${result}`;
-//         })
-//         .catch(err => console.log(err));
-// }
+function dateSelector(event) {
+    setLectionary(event.target.value);
+}
